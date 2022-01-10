d1495d7279922391f623b80aba867c297c08ec1a
Murad Satbi <murad.satbi@mail.ru>
2022-01-10 23:59:44 +0300

    Создание окна PyGame и обработка ввода

--- alien_invasion.py
+++ alien_invasion.py
@@ -7,3 +7,25 @@ class AlienInvasion:
     """Класс для управления ресурсами и поведением игры"""
 
     def __init__(self):
+        """Инициализирует игру и создает игровые ресурсы."""
+        pygame.init()
+
+        self.screen = pygame.display.set_mode((1200, 800))
+        pygame.display.set_caption("Alien Invasion")
+
+    def run_game(self):
+        """Запуск основного цикла игры."""
+        while True:
+            # отслеживание событий клавиатуры и мыши.
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+
+            # отображение последнего прорисованного экрана
+            pygame.display.flip()
+
+
+if __name__ == 'main':
+    # создание экземпляра и запуск игры.
+    ai = AlienInvasion()
+    ai.run_game()
